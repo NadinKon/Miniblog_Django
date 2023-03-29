d444084392d6280a91608a4087203c3e3f543f20
NadinKon <nadinkahf@gmail.com>
2023-03-29 19:28:00 +0300

    weather

--- myblog/blog/form.py
+++ myblog/blog/form.py
@@ -5,3 +5,9 @@ class CommentsForm(forms.ModelForm):
     class Meta:
         model = Comments
         fields = ('name', 'email', 'text_comments')
+
+
+# class CityForm(forms.ModelForm):
+#     class Meta:
+#         model = City
+#         fields = ('name')
